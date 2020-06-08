3dce165fd0be9e9d18578aa0fd1010f6c1e957a6
Alvaro Linares <alvarolinares@gmail.com>
2020-06-08 20:49:26 -0300

    Updates

---
+++ precache-manifest.2dc7f589350cdb0751570b206b8f1c08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465898002f882c5d481c2690acdb70c0",
+    "url": "/deviget-reddit-top/index.html"
+  },
+  {
+    "revision": "d764536c2e5959d4875c",
+    "url": "/deviget-reddit-top/static/css/main.43bf3a55.chunk.css"
+  },
+  {
+    "revision": "24bb918cb1424cc6f770",
+    "url": "/deviget-reddit-top/static/js/2.4db065b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deviget-reddit-top/static/js/2.4db065b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d764536c2e5959d4875c",
+    "url": "/deviget-reddit-top/static/js/main.017b310c.chunk.js"
+  },
+  {
+    "revision": "ee3e0a41906978fd68b2",
+    "url": "/deviget-reddit-top/static/js/runtime-main.1ca27d13.js"
+  }
+]);
