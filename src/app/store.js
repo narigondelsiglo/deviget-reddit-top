2b392001db0fc768f89f38e87e8fa7d5d27ade5c
Alvaro Linares <alvarolinares@gmail.com>
2020-06-08 13:57:43 -0300

    Added state persistance Based on Dan Abramov's solution: https://egghead.io/lessons/javascript-redux-persisting-the-state-to-the-local-storage

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import throttle from "lodash/throttle";
 import postsReducer from "./postsSlice";
+import { loadState, saveState } from "./localStorage";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     posts: postsReducer,
   },
+  preloadedState: loadState(),
 });
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      posts: store.getState().posts,
+    });
+  }, 1000)
+);
+
+export default store;
